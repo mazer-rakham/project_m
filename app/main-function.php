2043fb7ffca250f7fcfd99004d297e23907750aa
Shawn Flock <sflock@navicamls.net>
2017-01-26 13:31:27 -0500

    so confused

--- app/main-function.php
+++ app/main-function.php
@@ -2,6 +2,6 @@
     $cards = file_get_contents('js/AllCards.json');
     $decodedCards = json_decode($cards, true);
     
-    echo '<pre>' . print_r($decodedCards, true) . '</pre>';
+    echo '<pre>' . print_r($decodedCards,true) . '</pre>';
 
 ?>
