66c113df068cb41c69e4b4b043adbb6330717c56
Shawn Flock <sflock@navicamls.net>
2017-02-17 15:19:37 -0500

    commiting

--- app/index.php
+++ app/index.php
@@ -2,24 +2,54 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/css/main-stylesheet.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <!--[if lt IE 7]>
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/main-stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <?php 
+        require 'main-function.php';
+        ?>
+    <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        <nav>
-        </nav>
-        <?php   
-            require 'main-function.php';
+    <nav id="main-nav">
+        <a>Your Cards</a>
+        <a>All Cards</a>
+        <a>Wish List</a>
+        <a>Your Decks</a>
+        <a>Deck Builder</a>
+        <a>Settings</a>
+        <h2>Card Total:<br />13244.44</h2>
+    </nav>
+    <section id="search-and-filter">
 
-        ?>
-    </body>
+    </section>
+    <article id="main-area">
+        <article id="all-cards-wrapper">
+            <?php
+             for ($i = 0; $i < $cardCount; $i++ ){
+               echo"<div class='cards'>" . $decodedCards[$fullCard[$i]]['name'] . "</div>";
+            }
+
+
+            ?>
+        </article>
+    </article>
+    <?php 
+        
+        require 'footer.php';
+        
+         ?>
+</body>
+
 </html>
--- app/main-function.php
+++ app/main-function.php
@@ -2,8 +2,8 @@
     $cards = file_get_contents('js/AllCards.json');
     $decodedCards = json_decode($cards, true);
     $fullCard = array_keys($decodedCards);
-    $cardsCount = count($decodedCards);
-    echo  print_r($decodedCards[0],true);
 
+    $cardCount = count($fullCard);
+   
 
 ?>
